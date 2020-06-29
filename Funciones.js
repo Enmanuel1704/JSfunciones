b8df244879d64bd40bfe1074954994067c7240e8
Enmanuel1704 <enmanuel1704@gmail.com>
2020-06-28 22:19:26 -0400

    Update Funciones.js

--- Funciones.js
+++ Funciones.js
@@ -14,3 +14,123 @@ function Con_A_Decimal(decimal,base){
     }
     console.log(res)
 }
+
+
+
+function consultarYO(){
+
+    var cedula = "402-2517884-3";
+
+    if (cedula.length == 13 && cedula != "" && cedula !="___-_______-_"){
+
+    $.ajax({
+
+       type:"POST",
+
+       dataType:"html",
+
+       url:"https://www.lanzate.com.do/formLanzate/inc/consultas_miembros.php",
+
+       data: "padron=true&cedula="+cedula,
+
+       success: function(resp){//document.write(resp);
+
+           $('#cedula_v').val(cedula);
+
+           json = $.parseJSON(resp);
+
+           
+
+           var i = 0;
+           console.log(json[i]);
+           {//inicio for
+
+               var NombreMiembro = json[i].Nombre;
+
+               var recinto_e = json[i].Recinto;
+
+               var colegio_e = json[i].Colegio;
+
+               var FechaNacimiento = json[i].Nacimiento;
+
+               var Genero = json[i].Sexo;
+
+               var idprovincia = json[i].id_provincia
+
+               var idmunicipio = json[i].id_municipio
+
+               var provincia = json[i].Provincia
+
+               var municipio = json[i].Municipio
+
+               var direccion_recinto = json[i].DireccionRecinto
+
+               var sector_paraje = json[i].Sector
+
+               var codigoRecintoE = json[i].CodigoRecinto_e
+
+           }//final for
+
+           $(document).ready(function() {
+
+               $(".nombre").val(NombreMiembro).attr('readonly', true);
+
+               $(".recinto").val(recinto_e).attr('readonly', true);
+
+               $(".colegio-electoral").val(colegio_e).attr('readonly', true);
+
+               $(".nacimiento").val(FechaNacimiento);
+
+               $(".sexo").val(Genero);
+
+               $(".id_provincia").val(idprovincia);
+
+               $(".id_municipio").val(idmunicipio);
+
+               $(".Provincia").val(provincia);
+
+               $(".Municipio").val(municipio);
+
+               $(".DireccionRecinto").val(direccion_recinto);
+
+               $(".Sector").val(sector_paraje);
+
+               $(".CodigoRecinto_e").val(codigoRecintoE);
+
+               // $("[name=fullname]").val(nombreComp);
+
+               // $("#nivel option[value="+nivel+"]").attr('selected', true);
+
+               // $("[name=register]").attr('onClick','editar_usuarios('+idusuario+');');
+
+               // $("[name=register]").attr('value','Guardar');
+
+               if(resp != 2){
+
+                   $('.registro_miembro').fadeIn(300).css("display", "flex");
+
+                   $('.validar_miembro').fadeOut(300);
+
+                   $('.show_error').fadeIn(300).css("display", "none");
+
+               }else{
+
+                   $('.show_error').fadeIn(300).css("display", "flex");
+
+               }
+
+               // When clicking on the button close or the mask layer the popup closed
+
+           });
+
+       }//final function con variable resp
+
+   });
+
+}else{
+
+    alert('Digite su cédula');
+
+}
+
+}
